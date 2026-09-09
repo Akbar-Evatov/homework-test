5c52581193cef03b60c1108a32214e42c3d648f0
akbarevatov <a.evatov@newuu.uz>
2026-09-09 08:58:39 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: [
+    "@prisma/client",
+    ".prisma/client",
+    "@prisma/adapter-neon",
+    "@neondatabase/serverless",
+  ],
 };
 
 export default nextConfig;
